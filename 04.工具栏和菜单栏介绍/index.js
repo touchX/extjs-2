326f2ecdb2f573f450eb79e955d4c8b2bf1c24a7
潘朝田 <m18118819935@163.com>
2017-02-16 15:07:17 +0800

    修改代码

--- 04.工具栏和菜单栏介绍/index.js
+++ 04.工具栏和菜单栏介绍/index.js
@@ -4,26 +4,28 @@
 Ext.onReady(function(){
     // 1.认识Ext.toolbar.Toolbar
     // 1.1.简单的工具栏案例
-    var toolbar = new Ext.toolbar.Toolbar({  // 创建工具栏
-        renderTo : 'toolbar',
-        width : 600,
-    }) ;
-    // 在工具栏中添加菜单 ...
-    toolbar.add([
-        {text : '新建',handler:onButtonClick},
-        {text : '打开',handler:onButtonClick},
-        {text : '保存',handler:onButtonClick},
-        {text : '项目',handler:onButtonClick},
-        {text : '工具',handler:onButtonClick},
-        {text : '授权',handler:onButtonClick},
-    ]);
-
-    /**
-     * 执行回调函数
-     * @param btn
-     */
-    function onButtonClick(btn){
-        alert(btn.text);
-    }
-
+//  var toolbar = new Ext.toolbar.Toolbar({  // 创建工具栏
+//      renderTo : 'toolbar',
+//      width : '600%',
+//  }) ;
+//  // 在工具栏中添加菜单 ...
+//  toolbar.add([
+//      {text : '新建',handler:onButtonClick},
+//      {text : '打开',handler:onButtonClick},
+//      {text : '保存',handler:onButtonClick},
+//      {text : '项目',handler:onButtonClick},
+//      {text : '工具',handler:onButtonClick},
+//      {text : '授权',handler:onButtonClick},
+//  ]);
+//	
+//	
+//	
+//  /**
+//   * 执行回调函数
+//   * @param btn
+//   */
+//  function onButtonClick(btn){
+//      alert(btn.text);
+//  }
+	Ext.MessageBox.alert('系统提示','欢迎使用Hbuilder开发项目...');
 });
